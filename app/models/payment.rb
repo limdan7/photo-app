eb75d35660aab39c2805a7e6224b19dbd66dc852
limdan7 <limdan7@berkeley.edu>
2017-01-12 13:19:38 +0000

    Revised view for the stripe function

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -12,11 +12,11 @@ class Payment < ActiveRecord::Base
   
   def process_payment
     customer = Stripe::Customer.create email: email, card: token
-    
+          
     Stripe::Charge.create customer: customer.id, 
                           amount: 1000,
                           description: 'Premium',
                           currency: 'usd'
   end
-  
+
 end
